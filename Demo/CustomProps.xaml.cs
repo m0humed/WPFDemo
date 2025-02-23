ae34037f9bcd499f8702d8131cdd937a5fad11e2
m0humed <qandilmohammed832gmail.com>
2025-02-23 06:53:51 +0200

    Add Counter Prop and Dispatcher

--- Demo/CustomProps.xaml.cs
+++ Demo/CustomProps.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Demo
 {
@@ -22,6 +23,31 @@ namespace Demo
         public CustomProps()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer(TimeSpan.FromSeconds(1),DispatcherPriority.Normal 
+                , delegate {
+                    int newValue = 0;
+                    if(Counter == int.MaxValue)
+                        newValue = 0;
+                    else
+                        newValue = Counter + 1;
+                    SetValue(CounterProperty, newValue);
+
+
+                },Dispatcher);
         }
+
+
+
+        public int Counter
+        {
+            get { return (int)GetValue(CounterProperty); }
+            set { SetValue(CounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Counteroperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CounterProperty =
+            DependencyProperty.Register("Counteroperty", typeof(int), typeof(CustomProps), new PropertyMetadata(0));
+
+
     }
 }
